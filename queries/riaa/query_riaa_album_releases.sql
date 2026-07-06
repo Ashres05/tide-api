7b452ea9b3551953fcaa13975d0abfd7517881d9
Ashres05 <ashres05@gmail.com>
2026-07-06 13:57:55 -0700

    Removed album and song queries for redundancy among the other queries.

--- queries/riaa/query_riaa_album_releases.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    riaa_album_id,
-    title,
-    artist
-FROM current_dev.data.riaa_albums
-WHERE
-    NOT flagged_as_invalid;
--- queries/riaa/query_riaa_song_releases.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    riaa_song_id,
-    title,
-    artist
-FROM current_dev.data.riaa_songs
-WHERE
-    NOT flagged_as_invalid;
